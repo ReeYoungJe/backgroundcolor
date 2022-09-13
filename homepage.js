02414592f6c098dbcfd975eef3d38d1d4e35ad99
이영제 <zofhd2@naver.com>
2022-09-13 14:47:33 +0900

    랜덤 함수 생성

--- homepage.js
+++ homepage.js
@@ -1,3 +1,6 @@
-function chan (click){
-    $('p').html('클릭댐');
+function randomColor (){
+    let colorCode = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+    $('p').html('현재 색상은? ' + colorCode)
+    $('.randomColor').css('background', colorCode)
+
 }
